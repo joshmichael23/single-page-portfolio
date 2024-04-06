865fb9e199e2e6f8bbde208858771f252f41f17a
Your Name <you@example.com>
2024-04-07 01:28:35 +0800

    changes to skill

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -7,32 +7,32 @@ const projects = () => {
             
             <div>
                 <h2 className='skill'>HTML</h2>
-                <p className='experience'>4 Years Experience</p>
+                <p className='experience'>2 Years Experience</p>
             </div>
 
             <div>
                 <h2 className='skill'>CSS</h2>
-                <p className='experience'>4 Years Experience</p>
+                <p className='experience'>2 Years Experience</p>
             </div>
 
             <div>
                 <h2 className='skill'>Javascript</h2>
-                <p className='experience'>4 Years Experience</p>
+                <p className='experience'>2 Years Experience</p>
             </div>
 
             <div>
                 <h2 className='skill'>Accessibility</h2>
-                <p className='experience'>4 Years Experience</p>
+                <p className='experience'>1 Year Experience</p>
             </div>
 
             <div>
                 <h2 className='skill'>React</h2>
-                <p className='experience'>3 Years Experience</p>
+                <p className='experience'>1 Year Experience</p>
             </div>
 
             <div>
                 <h2 className='skill'>Sass</h2>
-                <p className='experience'>3 Years Experience</p>
+                <p className='experience'>1 Year Experience</p>
             </div>
 
 
